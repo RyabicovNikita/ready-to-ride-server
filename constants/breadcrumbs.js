9d316b712de2207a74fa35220d5fa35ac5b803f0
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-03-15 21:07:36 +0300

    add breadcrumbs for update /delete trips

--- constants/breadcrumbs.js
+++ constants/breadcrumbs.js
@@ -3,6 +3,9 @@ export const PASS_BREADCRUMBS = {
   CANCEL_TRIP: "/cancelTrip",
   CONFIRM_DRIVER_TRIP: "/confirmDriver",
   DELETE_DRIVER_FROM_TRIP: "/looseDriver",
+
+  UPDATE_TRIP: "/:id/edit",
+  DELETE_TRIP: "/:id/delete",
 };
 
 export const DRIVER_BREADCRUMBS = {
