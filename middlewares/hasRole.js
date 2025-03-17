f4235f19810ae9633606f01b7dab7087fdb6e05c
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-03-17 21:52:41 +0300

    delete console log

--- middlewares/hasRole.js
+++ middlewares/hasRole.js
@@ -1,7 +1,6 @@
 export const hasRole =
   (...roles) =>
   (req, res, next) => {
-    console.log(req.user);
     if (!roles.includes(req.user.role)) {
       res.send({ error: "Access denied" });
       return;
