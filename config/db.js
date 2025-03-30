b8551d6d080139521fa242d2d4ae284a6fa268f0
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-03-30 19:13:45 +0300

    delete options from pool

--- config/db.js
+++ config/db.js
@@ -4,10 +4,4 @@ const { Pool } = pg;
 
 config();
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+export const pool = new Pool();
