aa652a4b7b04404017e5a4aa364a5f29f422d0ec
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-03-30 17:47:00 +0300

    test

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ export const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  // password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  connectionString: `postgres://admin:Nikita_12345@db:5432/${process.env.DB_NAME}`
 });
