f5fc262d4c20a54b5d29b5c5ef7c19083f09e627
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-03-30 19:26:45 +0300

    1

--- config/db.js
+++ config/db.js
@@ -4,4 +4,10 @@ const { Pool } = pg;
 
 config();
 
-export const pool = new Pool();
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
