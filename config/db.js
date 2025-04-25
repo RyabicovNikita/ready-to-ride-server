7684a8bc5ba20ca3b11dbda6b855001bcb0cbd60
RyabicovNikita <RyabicovN1k@yandex.ru>
2025-04-25 22:28:07 +0300

    delete configs from pool

--- config/db.js
+++ config/db.js
@@ -5,10 +5,10 @@ const { Pool } = pg;
 config();
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
   
 });
